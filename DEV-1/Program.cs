5db975307aa10bdcf5e85f96f6c4169fa5859ae9
Volodko Julia <julia.volodko.2011@mail.ru>
2017-06-21 16:12:05 +0600

    ChangeNumbers DEV-1

---
+++ DEV-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ChangeNumbers
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int k = 0; k <= 100; k++)
+            {
+                String num = k.ToString();          
+                if (k % 3 == 0)
+                {
+                    Console.Write("3*" + k / 3);
+                }
+                else
+                {
+                    Console.Write(" " + num + " ");
+                }
+            }
+            Console.Write("\nPress any key...");
+            Console.ReadKey();
+        }
+    }
+}
