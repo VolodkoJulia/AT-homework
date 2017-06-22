debf1b975737882620b5e42197f808d20e2b0ae9
VolodkoJulia <julia.volodko.2011@mail.ru>
2017-06-22 15:56:24 +0600

    Update Program.cs

--- DEV-1_new/ChangeNumbersOutput/Program.cs
+++ DEV-1_new/ChangeNumbersOutput/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ChangeNumbers
-{
     class Program
     {
         static void Main(string[] args)
@@ -31,5 +24,5 @@ namespace ChangeNumbers
             Console.ReadKey();
         }
     }
-}
+
 
