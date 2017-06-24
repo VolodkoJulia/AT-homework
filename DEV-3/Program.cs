df9fb0dbbe68b61956fd5c515da33f6a2887b786
VolodkoJulia <julia.volodko.2011@mail.ru>
2017-06-24 20:37:40 +0600

    Update Program.cs

--- DEV-3/Program.cs
+++ DEV-3/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-//Thr Implementation of the Fibonacci sequence
+//The Implementation of the Fibonacci sequence
 
 class FibonacciSequence
 
