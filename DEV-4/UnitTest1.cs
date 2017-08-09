63ae7f64f297e601d638df61406acaeb569a5d94
Volodko Julia <julia.volodko.2011@mail.ru>
2017-08-09 12:00:55 +0600

    All code was changed

---
+++ DEV-4/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Numsequence_new;
+namespace SequenceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsNondecreasing()
+        {
+            int[] array = { 3, 5, 7 };
+            bool res = new SequenceAnalysis().CheckSequence(array);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void IsNotNonDecreasing()
+        {
+            int[] array = { 3, 2, 7 };
+            bool res = new SequenceAnalysis().CheckSequence(array);
+            Assert.AreEqual(false, res);
+        }
+    }
+}
