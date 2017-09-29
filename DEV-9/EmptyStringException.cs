9a42ac0c29578139d90472e67ce8e3e147a8c5d4
Volodko Julia <julia.volodko.2011@mail.ru>
2017-09-29 20:33:26 +0300

    Add comments for exception class

--- DEV-9/EmptyStringException.cs
+++ DEV-9/EmptyStringException.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 
 namespace RandomChangeTheSymbolsInString
-{
+{  // For empty string which are passed to the  methode for substitute the part of string
   class EmptyStringException : Exception
   {
+    //Change the Message from class of inheritor 
     public override string Message
     {
       get
