b250943f6d1c0b875f2d170a116302b5eae73a72
Volodko Julia <julia.volodko.2011@mail.ru>
2017-10-02 14:12:08 +0300

    The duplicate elements in array

---
+++ DEV-10/ElementChecker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using static System.Math;
+
+namespace RepeatElementInArray
+{ /// <summary>
+  /// For checking elements on uniqueness.
+  /// </summary>
+  class ElementChecker
+  {
+    const double EPSILON = 10e-2;
+    /// <summary>
+    /// For checking elements on uniqueness.
+    /// </summary>
+    /// <param name="array"> 
+    /// The array which we need to check.
+    /// </param>
+    /// <returns>
+    /// An array just with duplicate elements.
+    /// </returns>
+    public ArrayList CheckElements(double[][] array)
+    {
+      ArrayList arrayAfterCheck = new ArrayList();
+      for (int i = 0; i < array.Length; i++)
+      {
+        for (int j = 0; j < array[i].Length; j++)
+        {
+          int count = 0;
+          for (int k = 0; k < array.Length; k++)
+          {
+            for (int l = 0; l < array[k].Length; l++)
+            {
+              if (Abs(array[i][j] - array[k][l]) < EPSILON)
+              {
+                count++;
+                break;
+              }
+            }                        
+          }
+          if (count >= 2)
+          {
+            for (int checker = 0; checker < arrayAfterCheck.Count; checker++)
+            {
+              if (Abs((double)arrayAfterCheck[checker] - array[i][j]) < EPSILON)
+              {
+                arrayAfterCheck.Remove(array[i][j]);
+              }
+            }
+            arrayAfterCheck.Add(array[i][j]);
+            break;
+          }
+        }
+      }
+      return arrayAfterCheck;
+    }
+  }
+}
+
---
+++ DEV-10/EntryPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using static System.Console;
+
+namespace RepeatElementInArray
+{ 
+  /// <summary>
+  /// The entry point of the program.
+  /// </summary>
+  class EntryPoint
+  {
+    /// <summary>
+    /// For calling the methods to check array elements for repeatability
+    /// and to display an array before verification and after it.
+    /// </summary>
+    static void Main()
+    {
+      GeneratorOFArray generatorOfArray = new GeneratorOFArray();
+      double[][] afterArrayGeneration = generatorOfArray.FillArray();
+
+      OutputerOfArrays outputer = new OutputerOfArrays();
+      outputer.OutputDoubleArray(afterArrayGeneration);
+
+      ElementChecker elementChecker = new ElementChecker();
+      ArrayList resultArray = elementChecker.CheckElements(afterArrayGeneration);
+
+      ExistenceDuplicateElements existElement = new ExistenceDuplicateElements();
+      bool unique = existElement.CheckNonuniqueElement(resultArray);
+      if (unique)
+      {
+        Write("\nThere are no any duplicate elements.");
+      }
+
+      OutputerOfArrays outputerOfArrays = new OutputerOfArrays();
+      outputerOfArrays.OutputArrayList(resultArray);
+
+      ReadKey();
+    }
+  }
+}
