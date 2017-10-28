067196acb3c5ad742c58c711cf4eded05788de29
Volodko Julia <julia.volodko.2011@mail.ru>
2017-10-28 13:50:00 +0300

    Changed: else if -> Replace(); while(...); relocation methods to other classes.

--- DEV-11/DictionaryFiller.cs
+++ DEV-11/DictionaryFiller.cs
@@ -1,52 +1,21 @@
-﻿using static System.Console;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
-namespace TransformFromCirillicToLatin
-{ /// <summary>
-  ///  For filling the dictionaries and output the result on display.
-  /// </summary>
+namespace TransformWords
+{ 
+  // For filling the dictionaries.
   class DictionaryFiller
   {
-    /// <summary>
-    /// For filling the dictionaries.
-    /// </summary>
-    /// <param name="dictionary">
-    /// The dictionary, which you  want to fill.
-    /// </param>
-    /// <param name="reader">
-    /// The file from a specific directory for filling the dictionary.
-    /// </param>
-    /// <returns>
-    /// The full dictionary.
-    /// </returns>
     public Dictionary<string, string> FillTheDictionaries(Dictionary<string, string> dictionary, StreamReader reader)
     {
-      while (!reader.EndOfStream)
+      string line = null;
+      while ((line = reader.ReadLine()) != null)
       {
-        string line = reader.ReadLine();
-        if (reader.EndOfStream)
-        {
-          break;
-        }
         string[] pair = line.Split('-');
         dictionary.Add(pair[0], pair[1]);
       }
       reader.Close();
       return dictionary;
-    }
-
-    /// <summary>
-    /// To display a result strings.
-    /// </summary>
-    /// <param name="resultArray"></param>
-    public void PrintResultStrings(string resultString)
-    {
-      foreach (var symbol in resultString)
-      {
-        Write(symbol);
-      }
     }
   }
 }
--- DEV-11/EntryPoint.cs
+++ DEV-11/EntryPoint.cs
@@ -4,21 +4,24 @@ using System.IO;
 using System.Text;
 using static System.Console;
 
-namespace TransformFromCirillicToLatin
+namespace TransformWords
 {
-  /// <summary>
-  /// For colling the method to transfer from one language to another one.
-  /// </summary>
+  // The entry point of the programm.
   class EntryPoint
   {
-    const string PATHLATTOCYR = @"E:\Desktop\DicLatCirDown.txt";
-    const string PATHCYRTOLAT = @"E:\Desktop\DictCirLatDown.txt";
-    const string MENU = "Chose the language, on wich you want to translate:";
+    const string PATHLATTOCYR = @"...path to you Latin-Cyrillic dictionary: DicLatCirDown.txt...";
+    const string PATHCYRTOLAT = @"...path to you Cyrillic-Latin dictionary: DictCirLatDown.txt";
+    const string MENU = "\nChose the language, on wich you want to translate:";
     const string FIRSTCHOICE = "\n1 - To Cyrillic.";
     const string SECONDCHOICE = "\n2 - To Latin.";
     const string ENTERTHESTRING = "Enter the string:";
     const string ONEMORETIME = "\n3 - To enter again.\n >>";
+    const string FROMCYRTOLAT = "\nThe new word from Cyrillic to Latin:\n";
+    const string FROMLATTOCYR = "\nThe new word from Latin to Cyrillic:\n";
+    const string TOCYRRILIC = "1: To Cyrillic";
+    const string TOLATIN = "2: To Latin";
 
+    // For colling the right method by user request to transfer from one language to another one.
     static void Main()
     {
       Dictionary<string, string> dictionaryLatCyr = new Dictionary<string, string>();
@@ -28,43 +31,33 @@ namespace TransformFromCirillicToLatin
       DictionaryFiller dictionaryFiller = new DictionaryFiller();
       dictionaryLatCyr = dictionaryFiller.FillTheDictionaries(dictionaryLatCyr, readerLatCyr);
       dictionaryCyrLat = dictionaryFiller.FillTheDictionaries(dictionaryCyrLat, readerCyrLat);
-
       TransferToAnotherLanguage transform = new TransferToAnotherLanguage();
-      string forTranslateToCyrrilic = null;
-      string forTranslateToLatin = null;
-      string resultArrayFromLat = null;
-      string resultArrayFromCir = null;
-      int languageChoice;
-      bool key = true;
-      while(key == true)
+
+      bool choice = true;
+      while(choice == true)
       {
+        choice = false;
         Write(MENU);
         Write(FIRSTCHOICE);
         Write(SECONDCHOICE);
         Write(ONEMORETIME);
 
-        languageChoice = Int32.Parse(ReadLine());
-        switch (languageChoice)
+        switch (Int32.Parse(ReadLine()))
         {
           case 1:
-            WriteLine("1: To Cyrillic");
+            WriteLine(TOCYRRILIC);
             WriteLine(ENTERTHESTRING);
-            forTranslateToCyrrilic = ReadLine();
-            resultArrayFromLat = transform.TranslateToAnotherLanguage(dictionaryLatCyr, forTranslateToCyrrilic);
-            Write("The new word from Latin to Cyrillic:\n");
-            dictionaryFiller.PrintResultStrings(resultArrayFromLat);
-            key = true;
+            transform.PrintResultString(transform.GetWordsAndTranslateIt(dictionaryLatCyr));
+            choice = true;
             break;
           case 2:
-            Console.WriteLine("2: To Latin");
+            WriteLine(TOLATIN);
             WriteLine(ENTERTHESTRING);
-            forTranslateToLatin = ReadLine();
-            resultArrayFromCir = transform.TranslateToAnotherLanguage(dictionaryCyrLat, forTranslateToLatin);
-            Write("\nThe new word from Latin to Cirillic:\n");
-            dictionaryFiller.PrintResultStrings(resultArrayFromCir);
-            key = true;
+            transform.PrintResultString(transform.GetWordsAndTranslateIt(dictionaryCyrLat));
+            choice = true;
             break;
           case 3:
+            choice = true;
             break;
         }
       }
